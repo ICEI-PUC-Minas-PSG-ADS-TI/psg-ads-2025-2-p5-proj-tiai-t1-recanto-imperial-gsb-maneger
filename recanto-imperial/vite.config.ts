7eddf69e11bcc3e34db9141c388302ddae9eb1aa
Luís Nicodemos <136287691+Luis-Nicodemos@users.noreply.github.com>
2025-12-10 23:59:50 -0300

    Update vite.config.ts

--- recanto-imperial/vite.config.ts
+++ recanto-imperial/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
+        //verificar o localhost selecionado
         target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
